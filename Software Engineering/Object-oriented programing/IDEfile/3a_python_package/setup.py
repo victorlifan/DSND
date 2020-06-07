e6bc375a0b8d8455f77e6cb9b97457bcabe42d0b
Fan_Li <victorlifan@vip.qq.com>
2020-06-06 19:37:57 -0500

    rearrange

---
+++ Software Engineering/Object-oriented programing/IDEfile/3a_python_package/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions',
+      version='1.0',
+      description='Gaussian distributions',
+      package=['distribution'],
+      zip_safe=False)
---
+++ Software Engineering/Object-oriented programing/IDEfile/5_exercise_upload_to_pypi/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='pypiex_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['pypiex_distributions'],
+      zip_safe=False)
